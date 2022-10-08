3dba29ae8a5a605176b6862201301a890502d833
Alfiya Paukova <alfiya.paukova92@gmail.com>
2022-10-08 09:41:22 +0300

    new commit

--- src/main/java/ru/netology/DZ_10_Radioman/service/Radio.java
+++ src/main/java/ru/netology/DZ_10_Radioman/service/Radio.java
@@ -1,4 +1,68 @@
-package ru.netology.statistic;
+package ru.netology.DZ_10_Radioman.service;
 
 public class Radio {
+
+    protected int currentRadioStation; //поле - текущая станция
+    protected int currentVolume; // поле - текущая громкость
+
+    public void setCurrentRadioStation(int currentRadioStation) {
+        if (currentRadioStation < 0) {
+            return;
+        }
+        if (currentRadioStation > 9) {
+            return;
+        }
+        this.currentRadioStation = currentRadioStation;
+    }
+
+    public void setVolume(int currentVolume) {
+        if (currentVolume < 0) {
+            return;
+        }
+        if (currentVolume > 10) {
+            return;
+        }
+        this.currentVolume = currentVolume;
+    }
+
+    public void nextRadioStation() {
+        if (currentRadioStation < 9) {
+            currentRadioStation++;
+        } else {
+            currentRadioStation = 0;
+        }
+    }
+
+    public void nextVolume() {
+        if (currentVolume < 10) {
+            currentVolume++;
+        } else {
+            currentVolume = 0;
+        }
+    }
+
+    public void prevRadioStation() {
+        if (currentRadioStation > 0) {
+            currentRadioStation--;
+        } else {
+            currentRadioStation = 9;
+        }
+    }
+
+    public void prevVolume() {
+        if (currentVolume > 0) {
+            currentVolume--;
+        } else {
+            currentVolume = 10;
+        }
+    }
+
+
+    public int getRadioStationNumber() {
+        return currentRadioStation;
+    }
+
+    public int getVolume() {
+        return currentVolume;
+    }
 }
--- src/test/java/ru/netology/DZ_10_Radioman/service/RadioTest.java
+++ src/test/java/ru/netology/DZ_10_Radioman/service/RadioTest.java
@@ -1,2 +1,320 @@
-package PACKAGE_NAME;public class RadioTest {
+package ru.netology.DZ_10_Radioman.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class RadioTest {
+
+    @Test
+    public void shouleSetRadioStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(9);
+
+        int expected = 9;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadioStation_10() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(10);
+
+        int expected = 0;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadio_0_Station() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(0);
+
+        int expected = 0;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadio_1_Station() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(1);
+
+        int expected = 1;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadio_8_Station() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(8);
+
+        int expected = 8;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadioMinusOneStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(-1);
+
+        int expected = 0;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadio_25_Station() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(25);
+
+        int expected = 0;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToNextStation() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(6);
+
+        radio.nextRadioStation();
+
+        int expected = 7;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToNextStation_9() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(9);
+
+        radio.nextRadioStation();
+
+        int expected = 0;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToPrevStation_9() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(9);
+
+        radio.prevRadioStation();
+
+        int expected = 8;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToPrevStation_0() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(0);
+
+        radio.prevRadioStation();
+
+        int expected = 9;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToPrevStation_1() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(1);
+
+        radio.prevRadioStation();
+
+        int expected = 0;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void shouleSetVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(5);
+
+        int expected = 5;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetVolume_11() {
+        Radio radio = new Radio();
+        radio.setVolume(11);
+
+        int expected = 0;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSet_0_Volume() {
+        Radio radio = new Radio();
+        radio.setVolume(0);
+
+        int expected = 0;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadio_1_Volume() {
+        Radio radio = new Radio();
+        radio.setVolume(1);
+
+        int expected = 1;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadio_9_Volume() {
+        Radio radio = new Radio();
+        radio.setVolume(9);
+
+        int expected = 9;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadioMinusOneVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(-1);
+
+        int expected = 0;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouleSetRadio_25_Volume() {
+        Radio radio = new Radio();
+        radio.setVolume(25);
+
+        int expected = 0;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToNextVolume() {
+        Radio radio = new Radio();
+        radio.setVolume(6);
+
+        radio.nextVolume();
+
+        int expected = 7;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToNextVolume_10() {
+        Radio radio = new Radio();
+        radio.setCurrentRadioStation(10);
+
+        radio.nextVolume();
+
+        int expected = 0;
+        int actual = radio.getRadioStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToPrevVolume_10() {
+        Radio radio = new Radio();
+        radio.setVolume(10);
+
+        radio.prevVolume();
+
+        int expected = 9;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToPrevVolume_0() {
+        Radio radio = new Radio();
+        radio.setVolume(0);
+
+        radio.prevVolume();
+
+        int expected = 10;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToPrevVolume_1() {
+        Radio radio = new Radio();
+        radio.setVolume(1);
+
+        radio.prevVolume();
+
+        int expected = 0;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToPrevVolume_25() {
+        Radio radio = new Radio();
+        radio.setVolume(25);
+
+        radio.prevVolume();
+
+        int expected = 10;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void switchToPrevVolume_11() {
+        Radio radio = new Radio();
+        radio.setVolume(11);
+
+        radio.prevVolume();
+
+        int expected = 10;
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
+
